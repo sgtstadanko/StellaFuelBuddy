8a7ee4c2f7261ba7680fab77a00110815307bc65
William Bradley <billb@uab.edu>
2025-08-18 17:54:28 -0500

    Initial Commit

---
+++ StellaFuelBuddy/StellaFuelBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StellaFuelBuddyApp.swift
+//  StellaFuelBuddy
+//
+//  Created by William Bradley on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct StellaFuelBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
